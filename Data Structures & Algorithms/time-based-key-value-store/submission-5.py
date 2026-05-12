16d7e6d2d736e5b1a92b821435a4c64866ee9135
André Ferraz <oandreferraz@gmail.com>
2026-05-12 17:50:03 -0300

    Add: time-based-key-value-store - submission-5

---
+++ Data Structures & Algorithms/time-based-key-value-store/submission-5.py
@@ -0,0 +1,27 @@
+class TimeMap:
+
+    def __init__(self):
+        self.mapp = {}
+        
+
+    def set(self, key: str, value: str, timestamp: int) -> None:
+        if key not in self.mapp:
+            self.mapp[key] = []
+        self.mapp[key].append((timestamp, value))
+        
+
+    def get(self, key: str, timestamp: int) -> str:
+        if key not in self.mapp:
+            return ""
+        
+        previous = ""
+        
+        for i, (currentTimestamp, value) in enumerate(self.mapp[key]):
+            if currentTimestamp == timestamp:
+                return value
+            elif currentTimestamp < timestamp:
+                previous = value
+            else:
+                return previous
+        
+        return previous
